39c6b4810fd9df5ba13899837ac4dae2d1d9025d
tabassm1 <tabassm1@T40570-LR001.org.aalto.fi>
2018-06-19 21:21:25 +0300

    Initial commit.

---
+++ clarsgic.m
@@ -0,0 +1,34 @@
+function [estK, A,beta] = clarsgic(y,X, A,B,gama,maxp)
+%< Complex-valued LARS with generalized information criterion (GIC) for
+%  finding the sparsity level and respective solution.
+% Code by Muhammad Naveed Tabassum and Esa Ollila, Aalto University. <2018>
+
+[n,p] = size(X);
+if nargin < 6, maxp = min(n,p); end
+if nargin < 5, gama = 2; end
+
+scale_est = zeros(maxp,1);    % Unbiased scale estimate
+scale_est(1) = (y'*y)/n;      % For null, i.e., all zeros case
+nzB = B(:,2:end)~=0;
+beta = zeros(p,1);
+
+for k = 1:maxp-1
+    Xa = X(:, nzB(:,k));    % active-set of predictors
+    b_ls = Xa \ y;          % LSE for current active-set | B(nzB(:,k),k+1)
+    r = y - Xa*b_ls;        % Residual for current active-set
+    RSS = r'*r;
+    scale_est(k+1) = RSS / (n-k);
+end
+switch gama
+    case 0
+        c = log(n);                 % c-LARS-GIC0
+    case 1
+        c = log(n) * log(log(p));	% c-LARS-GIC1
+    otherwise
+        c = log(p) * log(log(n));	% c-LARS-GIC2
+end
+GIC = n*log(scale_est) + (0:maxp-1)'*c;	
+indx = find(GIC<=min(GIC), 1);
+estK = indx - 1;    % since we start from 0
+A = A(1:estK);
+beta(A) = X(:, A) \ y;
